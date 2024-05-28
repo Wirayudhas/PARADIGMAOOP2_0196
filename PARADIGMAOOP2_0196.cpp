74dd6465b45ce3f64b16235846009b7ab932598d
Wirayudhas <arturwirayuda@gmail.com>
2024-05-28 12:07:25 +0700

    menambahkan cout sebagai implementasi method

--- PARADIGMAOOP2_0196.cpp
+++ PARADIGMAOOP2_0196.cpp
@@ -20,4 +20,16 @@ class joko : public seseorang
 public:
 	//deklarasi
 	void pesan()
-	
+	{
+		//implementasi
+		cout << "pesan dari joko" << endl;
+	}
+};
+
+class lia : public seseorang
+{
+public:
+	void pesan()
+	{
+		cout << "pesan dari lia" << endl;
+	}
